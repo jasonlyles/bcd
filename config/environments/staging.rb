79f65d9ef50f8e74a8139f5dc3d6600fe8bbdffa
Jason Lyles <lylesjt@gmail.com>
2014-07-16 14:24:29 -0400

    Changing how config is set in staging and production env configs

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -34,7 +34,7 @@ BrickCity::Application.configure do
   config.serve_static_assets = true
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
-  config.action_controller.asset_host = "//#{AmazonConfig.config.asset_bucket}.s3.amazonaws.com"
+  config.action_controller.asset_host = "//#{ENV['BCD_S3_ASSET_BUCKET']}.s3.amazonaws.com"
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
