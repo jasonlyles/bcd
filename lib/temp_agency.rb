a74fe2afd90f587370abb760a766bab9a81a0fcd
Jason Lyles <lylesjt@gmail.com>
2014-07-21 20:43:49 -0400

    Adding logging to temp_agency

--- lib/temp_agency.rb
+++ lib/temp_agency.rb
@@ -14,24 +14,24 @@ module TempAgency
 
     def workers
       workers = @@heroku.get_ps(ENV['APP_NAME']).body.select { |ps| ps["process"] =~ /#{@ps_name}/ }
-      #puts "ACTIVE WORKERS FOR #{@ps_name}: #{workers.size}"
+      Rails.logger.debug("ACTIVE WORKERS FOR #{@ps_name}: #{workers.size}")
       workers.size
     end
 
     def workers=(qty)
-      #puts "SETTING WORKERS TO: #{qty} on PS #{@ps_name}"
+      Rails.logger.debug("SETTING WORKERS TO: #{qty} on PS #{@ps_name}")
       @@heroku.post_ps_scale(ENV['APP_NAME'], @ps_name, qty)
     end
 
     def job_count
-      #puts "QUEUE NAME: #{@queue_name}"
+      Rails.logger.debug("QUEUE NAME: #{@queue_name}")
       Resque.size(@queue_name)
     end
   end
 
   def after_perform_scale_down(*args)
     unless Rails.env == "development"
-      #puts "SCALING DOWN"
+      Rails.logger.debug("SCALING DOWN")
       @scaler = Scaler.new(queue_name: queue_name.to_s)
       @scaler.workers = 0 if @scaler.job_count.zero?
     end
@@ -39,7 +39,7 @@ module TempAgency
 
   def after_enqueue_scale_up(*args)
     unless Rails.env == "development"
-      #puts "SCALING UP"
+      Rails.logger.debug("SCALING UP")
       @scaler = Scaler.new(queue_name: queue_name.to_s)
       @scaler.workers = 1 if @scaler.job_count > 0 && @scaler.workers == 0
     end
