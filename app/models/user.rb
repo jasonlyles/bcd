a5099e1dc35fddd33458a9cdf3fe7f9ec5d8c13c
Jason Lyles <lylesjt@gmail.com>
2017-02-05 22:37:45 -0500

    Adding order follow up task and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ActiveRecord::Base
 
   has_many :orders
   has_many :line_items, :through => :orders
+  has_many :products, :through => :line_items
   has_many :authentications, :dependent => :destroy
   has_many :downloads
   has_one :cart
