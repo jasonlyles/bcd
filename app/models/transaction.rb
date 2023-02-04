5ba4c9d643087a20f2ae7d8bc41ed19d5bd1a180
Jason Lyles <lylesjt@gmail.com>
2023-02-04 14:24:00 -0500

    Remove unused class

--- app/models/transaction.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-# I don't think this class is being used. Commenting it out to see if any code notices it's gone.
-# class Transaction
-#   attr_accessor :email, :transaction_id, :request_id, :status, :date, :product, :quantity, :price
-# end
