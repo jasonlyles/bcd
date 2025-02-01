a176377be0300ef3cde8943ba24b64a7af8c62ab
Jason Lyles <lylesjt@gmail.com>
2025-02-01 10:23:28 -0500

    Address some Lighthouse issues (#88)

--- config/application.rb
+++ config/application.rb
@@ -61,7 +61,5 @@ module BrickCity
 
     # Send errors to our own defined routes instead of the public static html pages.
     config.exceptions_app = routes
-
-    config.action_view.image_loading = 'lazy'
   end
 end
