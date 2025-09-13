98c427184dea0f48e88255030dc3bf8cfbaf4d7e
Jason Lyles <lylesjt@gmail.com>
2025-09-13 17:04:47 -0400

    Some tweaks for schema.org

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -92,6 +92,7 @@ Rails.application.configure do
   # Set to catch mail via mailcatcher
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { address: 'localhost', port: 1025 }
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
 
   config.after_initialize do
     Bullet.enable = true
