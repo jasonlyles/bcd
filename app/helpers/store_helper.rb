a045da56d53d69d135908b4f5bb0274f3fbf51ce
Jason Lyles <lylesjt@gmail.com>
2026-03-15 22:41:04 -0400

    Work on some SEO stuff

--- app/helpers/store_helper.rb
+++ app/helpers/store_helper.rb
@@ -4,4 +4,9 @@ module StoreHelper
   def errant_cart_item?(item)
     session[:errant_cart_items].present? ? session[:errant_cart_items].flatten.include?(item) : false
   end
+
+  def product_header(product)
+    product_type = product.product_type.name.downcase == 'instructions' ? 'Instructions (Digital Download)' : product.product_type.name
+    "#{product.product_code} #{product.name} #{product_type}"
+  end
 end
