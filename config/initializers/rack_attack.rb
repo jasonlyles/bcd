b280a019c97e2bbdbcc35d62fa8b61991cb24fc2
Jason Lyles <lylesjt@gmail.com>
2026-09-07 12:42:41 -0400

    Trying another rack-attack config

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -6,15 +6,7 @@ class Rack::Attack
     ['127.0.0.1', '::1'].include?(req.ip)
   end
 
-  blocklist('Cloudflare WAF bypass') do |req|
-    # Skip in development and test environments so local dev doesn't get blocked
-    next false unless Rails.env.production?
-
-    # Block any request reaching Heroku that lacks Cloudflare's signature header
-    req.env['HTTP_CF_RAY'].blank?
-  end
-
-  ### 4. Custom Responses ###
+  ### 2. Custom Responses ###
   self.throttled_responder = lambda do |_env|
     [429, { 'Content-Type' => 'text/plain' }, ["Too many requests.\n"]]
   end
