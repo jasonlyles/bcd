a3f2c45688d21c462faf539997741f0b15da94e6
Jason Lyles <lylesjt@gmail.com>
2026-09-07 12:14:32 -0400

    Trying another rack-attack config

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -6,15 +6,6 @@ class Rack::Attack
     ['127.0.0.1', '::1'].include?(req.ip)
   end
 
-  blocklist('block-exploit-seekers') do |req|
-    # Exclude safelisted extensions first
-    next false if req.path =~ %r{\.(css|js|png|jpg|jpeg|gif|svg|webp|avif|ico|woff|woff2|ttf|otf|eot|map)$}i
-
-    req.path =~ %r{^/(wp-admin|wp-login|wp-content|phpmyadmin|\.env|\.git|config/|xmlrpc\.php)}i ||
-      req.path =~ %r{\.(php|asp|aspx|jsp|cgi|env|bak|sql|config)\b}i ||
-      req.path.include?('..')
-  end
-
   blocklist('Cloudflare WAF bypass') do |req|
     # Skip in development and test environments so local dev doesn't get blocked
     next false unless Rails.env.production?
